a28e1c6431229220e6b7b7d486f65f9f651c7563
Jonathan García <jona03g97@gmail.com>
2021-06-24 11:17:02 -0600

    Manejando secretos con dotenv

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 mongoose
   .connect(
-    "mongodb+srv://jona_gallery:jr2KvOeqgCjCXbo3@cluster0.jstvq.mongodb.net/gallery?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jstvq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
   )
   .then(() => console.log("DB connected"));
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = require("./router");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 
 app.use(cors());
 app.use(express.json());
